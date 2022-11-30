8afcef0d50ee6ec3c88fff865a20015277450a6c
Philip <carlphiliposcar@gmail.com>
2022-11-30 12:34:55 +1100

    updated the bash scripts

--- cmip5_scripts/job.sh
+++ cmip5_scripts/job.sh
@@ -1,14 +1,19 @@
-
+# submitting job: qsub job.sh 
+# check status: qstat -swx jobID
+# check utilisation rate (%gpu): nqstat_anu jobID
+# remove job from queue: qdel jobID 
 
 !/bin/bash
 
 PBS -l ncpus = 1
 PBS -l mem = 190 GB
-PBS -l jobfs = 200 GB # paralellisation
-PBS -q normal
+PBS -l jobfs = 200 GB # paralellisation (local disk on a compute node)
+PBS -q normal         # express, normal, copyq(internet access), hugemem, megamem, gpuvolta(volta gpu) 
 PBS -P k10
 PBS -l walltime = 2:00:00
-PBS -l storage = gdata/al33 + gdata/k10+scratch/k10 + gdata/hh5 # data: /g/data/al33/replicas/CMIP5/combined/ , scipts ran from: /g/data/k10/cb4968/phd/cmip5_scripts
+PBS -l storage = gdata/al33 + gdata/k10+scratch/k10 + gdata/hh5 # data: /g/data/al33/replicas/CMIP5/combined/
+                                                                # scipts run from: /g/data/k10/cb4968/phd/cmip6_scripts
+                                                                # anaconda environemt: /g/data/hh5/public/modules/conda/analysis3-unstable             
 PBS -l wd
 PBS -M <philip.blackberg@monash.edu> 
 PBS -m <abe>	
