047d5ab0d5aaee75c19596603d2a4af8ddc41452
Philip <carlphiliposcar@gmail.com>
2023-04-11 13:46:05 +1000

    changed some folder names

--- plotting/correlations/org_clouds/myFuncs.py
+++ plotting/correlations/org_clouds/myFuncs.py
@@ -263,147 +263,229 @@ def get_metric(metric, dataset, experiment='historical', home=home, resolution='
 
 
 
+def regrid_conserv(M_in):
+    # dimensions of model to regrid to
+    folder1 = '/g/data/al33/replicas/CMIP5/combined/LASG-CESS/FGOALS-g2/historical/day/atmos/day/r1i1p1/v20161204/pr'
+    fileName1 = 'pr_day_FGOALS-g2_historical_r1i1p1_19970101-19971231.nc'
+    path1 = folder1 + '/' + fileName1
+
+    folder2 = '/Users/cbla0002/Documents/data/CMIP5/ds_cmip5/FGOALS-g2'
+    fileName2 = 'FGOALS-g2_precip_historical.nc'
+    path2 = folder2 + '/' + fileName2
+    
+
+    try:
+        M_out = xr.open_dataset(path1)['pr'].sel(lat=slice(-30,30))
+    except FileNotFoundError:
+        try:
+            M_out = xr.open_dataset(path2)['precip'].sel(lat=slice(-30,30))
+        except FileNotFoundError:
+            print(f"Error: no file at {path1} or {path2}")
 
 
+    # dimensions
+    dlat = M_in.lat.data[1]-M_in.lat.data[0]
+    dlon = M_in.lon.data[1]-M_in.lon.data[0]
+    latBnds = (M_in.lat.data-(dlat/2), M_in.lat.data+(dlat/2))
+    lonBnds = (M_in.lon.data-(dlon/2), M_in.lon.data+(dlon/2))
+    lat = np.mean(latBnds, axis=0)
+    lon = np.mean(lonBnds, axis=0)
+    # area of gridboxes as fraction of earth surface area
+    area_wlat = np.cos(np.deg2rad(lat))*dlat*np.pi/(4*180^2)
 
+    dlat_n = M_out.lat.data[1]-M_out.lat.data[0]
+    dlon_n = M_out.lon.data[1]-M_out.lon.data[0]
+    latBnds_n = (M_out.lat.data-(dlat_n/2), M_out.lat.data+(dlat_n/2))
+    lonBnds_n = (M_out.lon.data-(dlon_n/2), M_out.lon.data+(dlon_n/2))
+    lat_n = np.mean(latBnds_n, axis=0)
+    lon_n = np.mean(lonBnds_n, axis=0)
 
+    # weights
+    Wlat = np.zeros([len(lat_n), len(lat)])
+    for i in np.arange(0,len(lat_n)):
+        latBoxMin_n = latBnds_n[0][i]
+        latBoxMax_n = latBnds_n[1][i]
 
+        # gridboxes that are atleast partially overlapping with iteration gridbox
+        J = (latBnds[0]<=latBoxMax_n)*(latBnds[1]>= latBoxMin_n)*area_wlat
 
+        # including fractional area component contribution
+        I = J*(latBnds[1]-latBoxMin_n)/dlat
+        K = J*(latBoxMax_n-latBnds[0])/dlat
+        II = np.min([I,J,K], axis=0)
 
+        # weights from individual gridboxes contributing to the new gridbox as fraction of the total combined area contribution
+        Wlat[i,:] = II/np.sum(II)
 
+    Wlat = xr.DataArray(
+        data = Wlat,
+        dims = ['lat_n', 'lat']
+        )
 
+    Wlon = np.zeros([len(lon_n), len(lon)])
+    for i in np.arange(0,len(lon_n)):
+        lonBoxMin_n = lonBnds_n[0][i]
+        lonBoxMax_n = lonBnds_n[1][i]
 
+        # gridboxes that are atleast partially overlapping with iteration gridbox
+        J = (lonBnds[0]<=lonBoxMax_n)*(lonBnds[1]>= lonBoxMin_n)*1
 
+        # Including fractional area component contribution
+        I = J*(lonBnds[1]-lonBoxMin_n)/dlon
+        K = J*(lonBoxMax_n-lonBnds[0])/dlon
+        L = J*(lonBoxMax_n-lonBnds[0]+360)/dlon
+        II = np.min([I,J,K,L], axis=0)
 
+        # weights from individual gridboxes contributing to the new gridbox as fraction of the total combined area contribution
+        Wlon[i,:] = II/np.sum(II)
 
+    Wlon = xr.DataArray(
+        data = Wlon,
+        dims = ['lon_n', 'lon']
+        )
 
+    # interpolation
+    if ('plev' or 'lev') in M_in.dims:
+        if 'lev' in M_in.dims:
+            M_n = M_n.rename({'lev': 'plev'})
 
+        M_n = xr.DataArray(
+            data = np.zeros([len(M_in.time.data), len(M_in.plev.data), len(lat_n), len(lon_n)]),
+            dims = ['time', 'plev', 'lat_n', 'lon_n'],
+            coords = {'time': M_in.time.data, 'plev': M_in.plev.data, 'lat_n': M_out.lat.data, 'lon_n': M_out.lon.data},
+            attrs = M_in.attrs
+            )
 
+        for day in np.arange(0,len(M_in.time.data)):
+            
+            M_Wlat = xr.DataArray(
+            data = np.zeros([len(M_in.plev), len(lat_n), len(lon)]),
+            dims = ['plev', 'lat_n', 'lon']
+            )
 
-def plot_format_multiple(datasets, variable, timeMean_options, experiments, data):
-    absolute_limits = True
-    quantile_low = 0
-    quantile_high = 1
-    if absolute_limits:
-        vmin, vmax = [], []
-        for dataset in datasets:
+            for i in range(0, len(Wlat.lat_n)):
+                M_Wlat[:,i,:] = (M_in.isel(time=day) * Wlat[i,:]).sum(dim='lat', skipna=True) / (M_in.isel(time=day).notnull()*1*Wlat[i,:]).sum(dim='lat')
+                
+            for i in range(0, len(Wlon.lon_n)):
+                M_n[day,:,:,i] = (M_Wlat * Wlon[i,:]).sum(dim='lon', skipna=True) / (M_Wlat.notnull()*1*Wlon[i,:]).sum(dim='lon')
 
-
-
-            vmin = np.append(vmin, np.quantile(y, quantile_low))
-            vmax = np.append(vmax, np.quantile(y, quantile_high))
-
-        vmin = np.min(vmin)
-        vmax = np.max(vmax)
 
     else:
-        vmin, vmax = None, None 
+        M_n = xr.DataArray(
+            data = np.zeros([len(M_in.time.data), len(lat_n), len(lon_n)]),
+            dims = ['time', 'lat_n', 'lon_n'],
+            coords = {'time': M_in.time.data, 'lat_n': M_out.lat.data, 'lon_n': M_out.lon.data},
+            attrs = M_in.attrs
+            )
 
+        for day in np.arange(0,len(M_in.time.data)):
 
-    fig= plt.figure(figsize=(22.5,17.5))
-    title = '{} spatial mean of {} field from model:{}, experiment:{}'.format(timeMean_options[0], variable, dataset, experiments[0])
+            M_Wlat = xr.DataArray(
+            data = np.zeros([len(lat_n), len(lon)]),
+            dims = ['lat_n', 'lon']
+            )
 
-    fig.suptitle(title, fontsize=18, y=0.95)
-
-    for i, dataset in enumerate(datasets):
-        ax= fig.add_subplot(5,4,i+1)
-        title = dataset
-
-
-        plot_timeseries(y, title=title, timeMean_option=timeMean_options, ax=ax, ymin=vmin, ymax=vmax)
-
-        if (len(datasets)-i)<=4:
-            xlabel = '{} [{} - {}]'.format(timeMean_options[0], str(data.isel(time=0).coords['time'].values)[:10], str(data.isel(time=-1).coords['time'].values)[:10])
-            plt.xlabel(xlabel)
-
-        if i== 0 or i==4 or i==8 or i==12 or i==16:
-            ylabel = 'Relative humidity [{}]'.format('%')
-            plt.ylabel(ylabel)
-
-    plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.15, hspace=0.3)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def find_limits(variable, datasets, resolutions, experiments, home=home, timeMean_option=[''], quantile_low=0, quantile_high=1, scene_type=''):
-
-#     vmin, vmax = [], []
-#     for dataset in datasets:
-#         for resolution in resolutions:
-#             for experiment in experiments:
+            for i in range(0, len(Wlat.lat_n)):
+                M_Wlat[i,:] = (M_in.isel(time=day) * Wlat[i,:]).sum(dim='lat', skipna=True) / (M_in.isel(time=day).notnull()*1*Wlat[i,:]).sum(dim='lat')
                 
-#                 data = get_dsvariable(variable, dataset, resolution, experiment, home)[variable]
-
-#                 if scene_type == 'example':
-#                     data = get_dsvariable(variable, dataset, resolution, experiment, home)[variable].isel(time=0)
-                
-#                 elif scene_type == 'experiment':
-#                     data = get_dsvariable(variable, dataset, resolution, experiment, home)[variable].mean(dim=('time'),keep_attrs=True)
-
-#                 elif scene_type == 'difference':
-#                     data_historical = get_dsvariable(variable, dataset, resolution, experiment='historical')[variable].mean(dim=('time'))
-#                     data_rcp85 = get_dsvariable(variable, dataset, resolution, experiment='rcp85')[variable].mean(dim=('time'))
-
-#                     if variable != 'tas':
-#                         tas_historical = get_dsvariable(variable='tas', dataset=dataset, resolution=resolution, experiment='historical')[variable].mean(dim=('time'))
-#                         tas_rcp85 = get_dsvariable(variable='tas', dataset=dataset, resolution=resolution, experiment='rcp85')[variable].mean(dim=('time'))
-#                         tas_difference = tas_rcp85 - tas_historical
-#                         data = (data_rcp85 - data_historical)/(data_historical*tas_difference)
-#                     else:
-#                         data = (data_rcp85 - data_historical)/data_historical
+            for i in range(0, len(Wlon.lon_n)):
+                M_n[day,:,i] = (M_Wlat * Wlon[i,:]).sum(dim='lon', skipna=True) / (M_Wlat.notnull()*1*Wlon[i,:]).sum(dim='lon')
 
 
-#                     if timeMean_option[0] == 'difference':
-#                         aWeights = np.cos(np.deg2rad(data_historical.lat))
-#                         data = (data_rcp85.weighted(aWeights).mean(dim=('lat','lon')) - data_historical.weighted(aWeights).mean(dim=('lat','lon')))/data_historical.weighted(aWeights).mean(dim=('lat','lon'))
+    M_n = M_n.rename({'lat_n': 'lat', 'lon_n': 'lon'})
+    
+    return M_n
 
 
-#                 if variable != 'tas' and len(np.shape(data))>2:
-#                     aWeights = np.cos(np.deg2rad(data.lat))
-#                     y= data.weighted(aWeights).mean(dim=('lat','lon'))
-#                 else:
-#                     y= data
 
-#                 if timeMean_option[0] == 'experiment':
-#                     y = y.mean(dim='time', keep_attrs=True)
 
-#                 if timeMean_option[0] == 'annual':
-#                     y = y.resample(time='Y').mean(dim='time', keep_attrs=True)
 
-#                 if timeMean_option[0] == 'seasonal':
-#                     y = y.resample(time='QS-DEC').mean(dim="time")
-#                     y = to_monthly(y)
-#                     y = y.rename({'month':'season'})
-#                     y = y.assign_coords(season = ["MAM", "JJA", "SON", "DJF"])
-#                     y = y.isel(year=slice(1, None))
 
-#                 if timeMean_option[0] == 'monthly':
-#                     if variable == 'tas' and datasets[0] == 'FGOALS-g2':
-#                         pass
-#                     else:
-#                         y = y.resample(time='M').mean(dim='time', keep_attrs=True)
 
-#                 if timeMean_option[0] == 'daily':
-#                     y = y
-                
-#                 vmin = np.append(vmin, np.quantile(y, quantile_low))
-#                 vmax = np.append(vmax, np.quantile(y, quantile_high))
 
-#     vmin = np.min(vmin)
-#     vmax = np.max(vmax)
 
-#     return vmin, vmax
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# -----------------------------------------------------------------------   Format for multiple plots   ----------------------------------------------------------------------------------- #
+
+# foramt for multiple plots
+# def plot_format_multiple(datasets, variable, timeMean_options, experiments, data):
+#     absolute_limits = True
+#     quantile_low = 0
+#     quantile_high = 1
+#     if absolute_limits:
+#         vmin, vmax = [], []
+#         for dataset in datasets:
+
+
+
+#             vmin = np.append(vmin, np.quantile(y, quantile_low))
+#             vmax = np.append(vmax, np.quantile(y, quantile_high))
+
+#         vmin = np.min(vmin)
+#         vmax = np.max(vmax)
+
+#     else:
+#         vmin, vmax = None, None 
+
+
+#     fig= plt.figure(figsize=(22.5,17.5))
+#     title = '{} spatial mean of {} field from model:{}, experiment:{}'.format(timeMean_options[0], variable, dataset, experiments[0])
+
+#     fig.suptitle(title, fontsize=18, y=0.95)
+
+#     for i, dataset in enumerate(datasets):
+#         ax= fig.add_subplot(5,4,i+1)
+#         title = dataset
+
+
+#         plot_timeseries(y, title=title, timeMean_option=timeMean_options, ax=ax, ymin=vmin, ymax=vmax)
+
+#         if (len(datasets)-i)<=4:
+#             xlabel = '{} [{} - {}]'.format(timeMean_options[0], str(data.isel(time=0).coords['time'].values)[:10], str(data.isel(time=-1).coords['time'].values)[:10])
+#             plt.xlabel(xlabel)
+
+#         if i== 0 or i==4 or i==8 or i==12 or i==16:
+#             ylabel = 'Relative humidity [{}]'.format('%')
+#             plt.ylabel(ylabel)
+
+#     plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.15, hspace=0.3)
+
+
+
+
+
+
+
+
 
 
 
