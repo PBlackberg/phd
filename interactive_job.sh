65f15215ccd0658cc355c5afb5c5b24d6491b71b
Philip Blackberg <philip.blackberg@monash.edu>
2023-06-11 13:02:22 +1000

    interactive_job change

--- interactive_job.sh
+++ interactive_job.sh
@@ -4,18 +4,27 @@
 #PBS -q normal         
 #PBS -P k10
 #PBS -l walltime=01:00:00
-#PBS -l storage=gdata/al33+gdata/oi10+gdata/ia39+gdata/k10+gdata/hh5      
+#PBS -l storage=gdata/al33+gdata/oi10+gdata/ia39+gdata/k10+gdata/hh5+home/565/cb4968      
 #PBS -l wd
 #PBS -M <philip.blackberg@monash.edu> 
 #PBS -I
 
 module use /g/data/hh5/public/modules
 module load conda/analysis3-unstable
-python /g/data/k10/cb4968/phd/test.py $PBS_NCPUS 
+python /home/565/cb4968/Documents/code $PBS_NCPUS 
+
+
+# was before:
+# python /g/data/k10/cb4968/phd/test.py
 
 
 # submitting job: 
+# qsub /home/565/cb4968/Documents/code/interactive_job.sh 
 # qsub /g/data/k10/cb4968/phd/interactive_job.sh 
+
+
+# alternaive inputs
+# -M <philip.blackberg@monash.edu> 
 
 # in ipython, run scripts from: 
 # cd /g/data/k10/cb4968/phd/
