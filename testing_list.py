f0f567614150da18f8c7f75c6c049beedd0a1388
Philip <carlphiliposcar@gmail.com>
2023-11-28 09:37:01 +1100

    trend plot

--- testing_list.py
+++ testing_list.py
@@ -1,14 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([np.nan, 2, 3])
 
 
+# plt.plot(a, b)
+# plt.show()
 
 
+print(np.nanmin(b))
 
-a = ['a', 'b', 
-     'c']
-
-
-
-
-
-
-print(a)
